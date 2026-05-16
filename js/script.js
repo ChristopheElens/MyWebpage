b7f1d45451d5233c509c57e97b8a0de52bc77fe1
ChristopheElens <elens.christophe@gmail.com>
2026-05-16 15:48:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 const currentYear = new Date().getFullYear();
 // toon huidige jaar nadien in het element met id "year" in footer
 document.getElementById("year").textContent = currentYear;
+
+// Script voor form-control: methode om ingevoerde gegevens te valideren wanneer het formulier ingevuld wordt
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
